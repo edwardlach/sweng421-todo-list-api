63e6f3dde5325e7ad9b6ecdb0c9fce9f848b07e6
Edward Lach <edwarddlach@gmail.com>
2021-12-02 15:35:01 -0500

    Added Future pattern logic to service network requests and the ChangePoller class

---
+++ to-do/Services/impl/ChangePoller.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using to_do.State;
+using System.Threading.Tasks;
+using System.Threading;
+using to_do.DTOs;
+namespace to_do.Services.impl
+{
+    public class ChangePoller : IChangePoller
+    {
+        private IUserService userService;
+        private Store store;
+
+        public ChangePoller(
+            Store store,
+            IUserService userService)
+        {
+            this.store = store;
+            this.userService = userService;
+        }
+
+        public void PollForChanges()
+        {
+            while(true)
+            {
+                var poll = Task<List<ChangeDTO.ChangeResponse>>.Run(() =>
+                {
+                    // TODO Make API request to get new changes for user instead
+                    // of below dummy code.
+                    ChangeDTO.ChangeResponse response = new ChangeDTO.ChangeResponse();
+                    List<ChangeDTO.ChangeResponse> changes = new List<ChangeDTO.ChangeResponse>();
+                    changes.Add(response);
+                    return changes;
+                });
+
+                if (poll.Result.Count > 0)
+                {
+                    poll.Result.ForEach(change => this.store.ChangeState.AddTo(change));
+                }
+                Thread.Sleep(10000);
+            }
+        }
+    }
+}
---
+++ to-do/State/ChangeState.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using to_do.DTOs;
+namespace to_do.State
+{
+    public class ChangeState : AbstractState<ChangeDTO.ChangeResponse>
+    {
+        public ChangeState(List<ChangeDTO.ChangeResponse> initialState) : base(initialState) {}
+
+    }
+}
