f7c65716e52a866ff9404aac855bd3c429ce70c5
Edward Lach <edwarddlach@gmail.com>
2021-12-14 19:53:41 -0500

    Attempting to make ChangePoller a timed background service

--- to-do/Services/impl/ChangePoller.cs
+++ to-do/Services/impl/ChangePoller.cs
@@ -5,12 +5,14 @@ using to_do.State.@abstract;
 using System.Threading.Tasks;
 using System.Threading;
 using to_do.DTOs;
+using Microsoft.Extensions.Hosting;
 namespace to_do.Services.impl
 {
-    public class ChangePoller : IChangePoller
+    public class ChangePoller : IChangePoller, IHostedService
     {
         private ISubscriptionService subscriptionService;
         private Store store;
+        private Timer _timer;
 
         public ChangePoller(
             Store store,
@@ -20,48 +22,52 @@ namespace to_do.Services.impl
             this.subscriptionService = subscriptionService;
         }
 
-        public Task PollForChanges()
+        public void PollForChanges(object state)
         {
-            var cancellationTokenSource = new CancellationTokenSource();
-            var token = cancellationTokenSource.Token;
+            CancellationToken token = (CancellationToken)state;
+            List<SubscriptionDTO.SubscriptionResponse> currrentSubscriptions
+                    = new List<SubscriptionDTO.SubscriptionResponse>();
 
-            return Task.Run(() =>
-            {
-                List<SubscriptionDTO.SubscriptionResponse> currrentSubscriptions
-                        = new List<SubscriptionDTO.SubscriptionResponse>();
-
-                new Observer<
-                        SubscriptionDTO.SubscriptionResponse,
-                        List<SubscriptionDTO.SubscriptionResponse>>(
-                    (subscriptions) =>
-                    {
-                        currrentSubscriptions = subscriptions;
-                        return subscriptions;
-                    })
-                    .Subscribe(this.store.SubscriptionState, this.store.SubscriptionState.SelectAll);
-
-                while (true)
+            new Observer<
+                    SubscriptionDTO.SubscriptionResponse,
+                    List<SubscriptionDTO.SubscriptionResponse>>(
+                (subscriptions) =>
                 {
+                    currrentSubscriptions = subscriptions;
+                    return subscriptions;
+                })
+                .Subscribe(this.store.SubscriptionState, this.store.SubscriptionState.SelectAll);
 
-                    List<ChangeDTO.ChangeResponse> changes = new List<ChangeDTO.ChangeResponse>();
-                    currrentSubscriptions.ForEach(s =>
-                    {
-                        changes.AddRange(this.subscriptionService
-                            .ReadSubscribedChanges(s.Id)
-                            .Result.Collection);
-                    });
-                    if (changes.Count > 0)
-                    {
-                        changes.ForEach(change => this.store.ChangeState.AddTo(change));
-                    }
-                    Thread.Sleep(10000);
-
-                    if (token.IsCancellationRequested)
-                    {
-                        break;
-                    }
+            while (true)
+            {
+                List<ChangeDTO.ChangeResponse> changes = new List<ChangeDTO.ChangeResponse>();
+                currrentSubscriptions.ForEach(s =>
+                {
+                    changes.AddRange(this.subscriptionService
+                        .ReadSubscribedChanges(s.Id)
+                        .Result.Collection);
+                });
+                if (changes.Count > 0)
+                {
+                    changes.ForEach(change => this.store.ChangeState.AddTo(change));
                 }
-            }, token);               
-        }      
+
+                if (token.IsCancellationRequested)
+                {
+                    break;
+                }
+            }             
+        }
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            this._timer = new Timer(PollForChanges, cancellationToken, TimeSpan.Zero, TimeSpan.FromSeconds(5));
+            return Task.CompletedTask;
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            return Task.CompletedTask;
+        }
     }
 }
